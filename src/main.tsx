c5e5bf46d9c03424e439168ef041ebb86305c9ab
waxer59 <hnboyano@gmail.com>
2023-05-30 00:31:15 +0200

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
